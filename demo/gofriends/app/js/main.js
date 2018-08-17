639c810750c1d1417589b0d2c5b407aaa9ef1815
Bogdan <sokolovskyi@yahoo.com>
2018-08-17 22:42:42 +0300

    header and mobile-menu

--- demo/gofriends/app/js/main.js
+++ demo/gofriends/app/js/main.js
@@ -1,5 +1,32 @@
 $(function() {
 
+	// Mobile menu
+	$("#menu").mmenu({
+		extensions: [
+			'widescreen',
+			'theme-white',
+			'effect-menu-slide',
+			'pagedim-black',
+			"effect-menu-zoom",
+			"effect-listitems-slide"
+		],
 
+		offCanvas: {
+			position: 'left',
+			zposition: 'front',
+		},
+
+		navbar: {
+			title: "Меню"
+		}
+	});
+
+	$(".toggle-mnu").click(function() {
+		$(this).toggleClass("on");
+	});
+	var api = $("#menu").data("mmenu");
+	api.bind("closed", function () {
+		$(".toggle-mnu").removeClass("on");
+	});
 
 });
--- demo/gofriends/gulpfile.js
+++ demo/gofriends/gulpfile.js
@@ -25,7 +25,7 @@ gulp.task('browser-sync', function() {
 });
 
 gulp.task('styles', function() {
-	return gulp.src('app/'+syntax+'/**/*.'+syntax+'')
+	return gulp.src(['app/'+syntax+'/**/*.'+syntax+'', 'app/'+syntax+'/*.css'])
 	.pipe(sass({ outputStyle: 'expand' }).on("error", notify.onError()))
 	//.pipe(rename({ suffix: '.min', prefix : '' }))
 	.pipe(autoprefixer(['last 15 versions']))
@@ -62,7 +62,7 @@ gulp.task('rsync', function() {
 });
 
 gulp.task('watch', ['styles', 'js', 'browser-sync'], function() {
-	gulp.watch('app/'+syntax+'/**/*.'+syntax+'', ['styles']);
+	gulp.watch('app/'+syntax+'/**/*.'+syntax+'',  ['styles']);
 	gulp.watch([/*'libs/!**!/!*.js',*/ 'app/js/main.js'], ['js']);
 	gulp.watch('app/*.html', browserSync.reload)
 });
