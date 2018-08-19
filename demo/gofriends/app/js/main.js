29ef05d8bfedf0e3bf519c18e91321ab70c68710
Bogdan <sokolovskyi@yahoo.com>
2018-08-19 18:08:10 +0300

    slider mobile

--- demo/gofriends/app/js/main.js
+++ demo/gofriends/app/js/main.js
@@ -211,13 +211,13 @@ $(function() {
 
 		sync1.owlCarousel({
 			items: 1,
-			slideSpeed: 2000,
+			slideSpeed: 5000,
 			nav: true,
 			autoplay: true,
 			dots: false,
 			loop: true,
 			responsiveRefreshRate: 200,
-			navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>', '<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
+			navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
 		}).on('changed.owl.carousel', syncPosition);
 
 		sync2
@@ -227,7 +227,7 @@ $(function() {
 			.owlCarousel({
 				items: slidesPerPage,
 				dots: false,
-				nav: true,
+				nav: false,
 				smartSpeed: 200,
 				slideSpeed: 500,
 				slideBy: slidesPerPage, //alternatively you can slide by 1, this way the active slide will stick to the first item in the second carousel
